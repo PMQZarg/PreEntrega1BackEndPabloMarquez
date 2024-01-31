506dbd7b3245cf25b75b321fccad4b7b31719395
PMQZArg <pmqz.ar@gmail.com>
2024-01-31 18:13:41 -0300

    PreEntregaBackEndPabloMarquez

--- app.js
+++ app.js
@@ -1,40 +1,21 @@
 import express from "express";
 import Index from "./src/router/indexRouter.js";
+import { ProductManager } from "./src/manager/productManager.js";
+import { CartManager } from "./src/manager/cartManager.js";
+import router from "./src/router/cartRouter.js";
 
 const app = express();
 const PORT = 8080;
 //middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+export const productManager = new ProductManager(pathProducts);
+export const cartManager = new CartManager(pathCarts);
+
 app.use("/api", Index);
-//Endpoint limitando consultas sobre productos
-
-app.get("api/products", async (req, res) => {
-  const productos = await productManager.getProducts();
-  res.json(productos);
-
-  let limit = parseInt(req.query.limit);
-  let productsLimited = [...productos];
-
-  if (!isNaN(limit) && limit > 0) {
-    limitedProducts = productsLimited.slice(o, limit);
-  }
-
-  res.json(limitedProducts);
-});
-
-app.get("/products/:pid", async (req, res) => {
-  let productId = parseInt(req.params.pid);
-  const productos = await productManager.getProductsById(productId);
-  //const product = products.find((p) => p.id === productId);
-  if (!productos) {
-    res
-      .status(404)
-      .json({ error: `Producto con Id ${productId} no encontrado` });
-  } else {
-    res.json(productos);
-  }
-});
+app.use("/api/products", routerProducts);
+app.use("/api/carts", routerCarts);
 
 app.listen(PORT, () => {
   console.log(`El servidor esta corriendo en http://localhost:${PORT}`);
--- src/manager/cartManager.js
+++ src/manager/cartManager.js
@@ -1,9 +1,78 @@
+
+import fs from "fs";
+import { ProductManager } from "../../app.js";
 export class CartManager {
-  async getCarts() {}
+  constructor() {
+    this.products = [];
+    this.path = "./src/data/cart.json";
+  }
 
-  async createCarts() {}
+  async getCarts() {
+    try {
+      const data = await fs.readFile(this.path, "utf8");
+      this.carts = JSON.parse(data);
+      console.log("El archivo se ha leido con éxito");
+    } catch (error) {
+      console.log("Error leyendo el archivo", error);
+    }
+    return this.carts;
+  }
 
-  async updateCarts() {}
+  getCartById(cartId) {
+    this.getCarts();
+    const product = this.carts.find((p) => cart.id === cartId);
+    if (cart === undefined) {
+      console.log(`El producto con el id ${id} no existe`);
+    } else return cart;
+  }
 
-  async deleteCarts() {}
+  setId() {
+    this.lastCartId = this.getLastCartId();
+    if (this.lastCartId === 0) this.lastCartId = 1;
+    else this.lastCartId++;
+    return this.lastCartId;
+  }
+  getLastCartId() {
+    if (this.carts.length === 0) return 0;
+    const lastCartId = this.carts[this.carts.length - 1].cartId;
+    console.log("Mi último Id es", this.lastCartId);
+    return lastCartId;
+  }
+  async updateCart(id, cartUpdated) {
+    await this.getCarts();
+    const existingCart = this.carts.find((product) => product.id === id);
+
+    if (existingCart === undefined) {
+      console.error(`El id ${id} no existe`);
+      return;
+    }
+    const indice = this.carts.findIndex((product) => product.id === id);
+    this.products[indice] = { id, ...cartUpdated };
+
+    try {
+      await fs.writeFile(this.path, JSON.stringify(this.carts));
+      console.log("Archivo actualizado con éxito");
+    } catch (error) {
+      console.error("No se ha podido actualizar el archivo", error);
+    }
+  }
+
+  deleteCart(cartId,id) {
+    this.getCarts();
+    if (this.products.find((product) => product.id) === undefined) {
+      console.error(`El id ${id} no existe`);
+      return;
+    }
+
+    const indice = this.products.findIndex((product) => product.id === id);
+    this.products.splice(indice, 1);
+    try {
+      fs.writeFileSync(this.path, JSON.stringify(this.products));
+      console.log("El producto ha sido borrado");
+    } catch (error) {
+      console.error("Error borrando el producto", error);
+    }
+  }
 }
+
+main();
